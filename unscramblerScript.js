32cbec25c10f380deffc9fed57848f91d8877826
KyleSaladin <kylesaladinprof@gmail.com>
2025-09-23 15:02:37 -0700

    Finished polishing the game
    I added the final core features, and now I am testing to see if fetch works through github.

--- unscramblerScript.js
+++ unscramblerScript.js
@@ -1,42 +1,34 @@
-const words = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
-    "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
-    "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde",
-    "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo",
-    "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti",
-    "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-    "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
-    "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland",
-    "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan",
-    "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho",
-    "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives",
-    "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco",
-    "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand",
-    "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine",
-    "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia",
-    "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino",
-    "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia",
-    "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka",
-    "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo",
-    "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine",
-    "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
-    "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"];
+fetch('Dictionaries/countries.json')
+    .then(res => res.json())
+    .then(data => {
+        const dictionary = data;
+        console.log(countries);
+    })
+    .catch(err => console.error(err));
 
 let playing = true;
 const countries = 195;
 let score = 0;
 let word = "";
+let guessed = true;
 
 function start() {
-    document.getElementById("startButton").innerText = "Next";
-    document.getElementById("guess").value = ""
-    playGame();
+    if (guessed == true) {
+        guessed = false;
+
+        document.getElementById("startButton").innerText = "Next";
+        document.getElementById("guess").value = ""
+        document.getElementById("correctAnswer").innerText = "";
+
+        playGame();
+    }
 }
 
 
 function playGame() {
 
     word = getRandomWord(words, 0, countries);
-    word.toLowerCase();
+    word = word.toLowerCase();
     console.log(word);
     let scrambled = scrambleWord(word);
 
@@ -55,24 +47,24 @@ function randomNumber(start, end) {
 
 function scrambleWord(word) {
     let scrambled = "";
+
     for (let i = 0; i < word.length; i++) {
         let rand = randomNumber(0, scrambled.length);
         scrambled = scrambled.slice(0, rand) + word[i] + scrambled.slice(rand)
     }
-    console.log(scrambled)
+
     return scrambled;
 }
 
 function onSubmit() {
+    if (guessed == true) { return; }
+
     let guess = document.getElementById("guess").value;
     guess.toLowerCase();
-    console.log(word + "===" + guess);
-    if (guess === word) {
-        score++;
-    }
-    else{
-        score = 0;
-    }
-    console.log(score);
+    guess === word ? score++ : score = 0;
+
+    guessed = true;
+
+    document.getElementById("correctAnswer").innerText = word;
     document.getElementById("myScore").innerText = "Score: " + score;
 }
